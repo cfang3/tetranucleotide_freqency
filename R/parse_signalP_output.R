b63fa18b7435da2dfc4ff8c4351d0418bdc56d40
cameron fang <cameronfang@camerons-MacBook.local>
2019-12-05 12:10:51 -0500

    changed name of the calc_tetranucleotide_freqs function

--- R/parse_signalP_output.R
+++ R/parse_signalP_output.R
@@ -13,11 +13,11 @@ source("R/calc_tetra_nuc.R")
 # Run one at a time to make function is working properly
 fn <- "data/GCF_000003135.1_ASM313v1_genomic.aa.fsa.out.neg"
 system.time({
-  test_obj <- calc_tetranucleotide_freqs(fn, discard.data = FALSE)
+  test_obj <- calc_tetra_nuc(fn, discard.data = FALSE)
 })
 
 # Get a character vector of all paths to the signalP output files (.neg files)
 signalP.output.path <- paste0("data/", signalP.output)
 
 # Run this for every signalP file you have and applies the function over the list of files.
-lapply(signalP.output.path, calc_tetranucleotide_freqs, save.file = TRUE, discard.data = FALSE)
+lapply(signalP.output.path, calc_tetra_nuc, save.file = TRUE, discard.data = FALSE)
