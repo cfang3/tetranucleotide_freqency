72cb251dd0a9d45cc50789b0567a9c694560dd06
cameron fang <cameronfang@camerons-MacBook.local>
2019-12-15 19:50:08 -0500

    final project

--- R/get_gg_title.R
+++ R/get_gg_title.R
@@ -6,14 +6,14 @@ get_gg_title <- function(signalP.out.fn) {
   #signalP_results <- read_fwf(file = signalP.out.fn, col_positions = fwf_empty(signalP.out.fn, skip = 1, col_names = colnames), skip = 2)
   
   # Extract the root of the signalP.out.fn file name to get the fna path
-  fsa.path <- str_extract(signalP.out.fn, ".+(?=(\\.out\\.neg)$)") # directory path to fsa file using stringr
-  fna.path <- str_replace(fsa.path, "(aa\\.fsa)$", "fna") # directory path to fna file using stringr
+  fsa.path <- stringr::str_extract(signalP.out.fn, ".+(?=(\\.out\\.neg)$)") # directory path to fsa file using stringr
+  fna.path <- stringr::str_replace(fsa.path, "(aa\\.fsa)$", "fna") # directory path to fna file using stringr
   
   # Read the fna file that has the organism name under attr("Annot")
   curr_fna <- seqinr::read.fasta(fna.path) # curr_fasta means "current fasta" because we'll call this function on a whole list of fasta files
   
   # Extract the organism's name from the attr of the fna file to be used in the PCA plot
-  ggplot.name <<- str_extract(attr(curr_fna[[1]],"Annot"),"(?<=\\s).{1,1000}(?=,\\s)")
+  ggplot.name <<- stringr::str_extract(attr(curr_fna[[1]],"Annot"),"(?<=\\s).{1,1000}(?=,\\s)")
  
 
   
